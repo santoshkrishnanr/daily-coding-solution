4fdab4445b108d7f21be918f404220bb1a1c7a3d
santoshkrishnanr <santoshkrishnanr@github.com>
2020-10-15 20:18:32 +0200

    initial test

--- day-1.py
+++ day-1.py
@@ -3,8 +3,9 @@ This problem was recently asked by Google.
 Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
 For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 Bonus: Can you do this in one pass?
+time 0.049 0.031
 """
-arr = [10, 15, 3, 7]
+arr = [10, 15, 3, 2]
 sum = 17
 
 def pairSum(arr, sum):
