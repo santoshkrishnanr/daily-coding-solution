ba63d9dc137289d5db5cc5c254c5f4ce3cb48a16
santoshkrishnanr <santoshkrishnanr@github.com>
2020-10-15 21:39:17 +0200

    with lesser run time and other given values

--- day-1.py
+++ day-1.py
@@ -3,19 +3,20 @@ This problem was recently asked by Google.
 Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
 For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 Bonus: Can you do this in one pass?
-time 0.049 0.031
+run time pairSum 0.049 -0.031
+run time for sanlist 0.029
 """
-arr = [10, 15, 3, 2]
+arr = [10, 15, 3, 7]
 sum = 17
-
-def pairSum(arr, sum):
-    hashTable = {} # Create a hash table
-    hashTable[arr[0]] = 0 # Add first element to hash table
-    for i in range(1, len(arr)): # Loop through array
-        currentElement = arr[i]
-        requiredElement = sum - currentElement # We require sum - current Element to exist in array
-        if requiredElement in hashTable:
+def sanlist(arr,sum):
+    lis= arr
+    s= sum
+    for i in range(0, len(lis)): 
+        requiredElement = s - lis[0]         #element to be searched  
+        lis.remove(lis[0])                   # remove the element 
+        if requiredElement in lis:           #compare with others 
             return True # return True if required element was found
     return False # return False if required element was not found
 
-print(pairSum(arr, sum)) # True
+print(sanlist(arr, sum))
+
