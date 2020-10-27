b0bf4c12ea7a743b0fc1a2eafe2a0f25daf57033
Santosh Krishnan R <46351924+santoshkrishnanr@users.noreply.github.com>
2020-10-27 08:12:18 +0100

    Update day-11.py
    correct question

--- day-11.py
+++ day-11.py
@@ -1,9 +1,10 @@
 
 """
-This problem was asked by Facebook.
-Given the mapping a = 1, b = 2, ... z = 26, and an encoded message, count the number of ways it can be decoded.
-For example, the message '111' would give 3, since it could be decoded as 'aaa', 'ka', and 'ak'.
-You can assume that the messages are decodable. For example, '001' is not allowed.
+This problem was asked by Twitter.
+Implement an autocomplete system. That is, given a query string s and a set of all possible query strings, return all strings in the set that have s as a prefix.
+For example, given the query string de and the set of strings [dog, deer, deal], return [deer, deal].
+
+Hint: Try preprocessing the dictionary into a more efficient data structure to speed up queries.
 """
 
 
